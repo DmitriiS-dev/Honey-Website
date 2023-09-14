136d4232144361c384ec5dfb13bc090d0b942c34
DmitriiUser <107858231+DmitriiUser@users.noreply.github.com>
2023-09-14 20:59:33 +0100

    Final Website
    Final version of the website with the About page completed

--- pages/about_page/about.js
+++ pages/about_page/about.js
@@ -1,5 +1,9 @@
 import Navbar from "../components/navbar/navbar";
 
+import styles from "./about.module.css";
+
+import Image from 'next/image';
+
 //Context API:
 
 
@@ -8,7 +12,29 @@ function AboutPage(){
     <>
       <Navbar></Navbar>
       <h1>About</h1>
-      <p>Our Raw Honey is unheated and unpasteurised, retaining all of the nutritional properties and benefits of honey. Our products are high in antioxidants and antibacterial qualities</p>
+      <div className={styles.container}>
+        <div className={styles.section}>
+          <Image className={styles.img} src="/images/close_up_honeycomb.jpg" alt="Close up honeycomb" width='300' height='300'/>
+          <div className={styles.text}>
+            <h4 className={styles.label}>Local Product</h4>
+            <p className={styles.description}>Produced in Kent, our honey captures the distinct flavours of this region's flora. It's enriched with the natural sugars found in local nectar sources, providing a genuine taste of our surroundings</p> 
+          </div> 
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h4 className={styles.label}>Honey Pureness</h4>
+            <p className={styles.description}>Our Raw Honey is unheated and unpasteurised, retaining all of the nutritional properties and benefits of honey. Our products are high in antioxidants and antibacterial qualities</p>
+          </div>
+          <Image className={styles.img} src="/images/honeycomb.jpg" alt="Zoomed in honeycomb" width='300' height='300'/>
+        </div>
+        <div className={styles.section}>
+          <Image className={styles.img} src="/images/top_honeycomb.jpg" alt="Top view honeycomb" width='300' height='300'/>
+          <div className={styles.text}>
+            <h4 className={styles.label}>Hayfeaver Relief</h4>
+            <p className={styles.description}>Our locally-produced Honey is celebrated for its remarkable ability to inhibit hayfever symptoms in our local area, making it a favorite choice for those seeking natural relief</p> 
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- pages/components/footer/footer.js
+++ pages/components/footer/footer.js
@@ -11,6 +11,10 @@ function Footer(){
         <p>Mouse hover icon: https://www.flaticon.com/free-icons/tap</p>
         <p>Honey Jar icon: https://www.flaticon.com/free-icons/label</p>
         <p>Box icon: https://www.flaticon.com/free-icons/collect</p>
+
+        <p>Close up honeycomb: https://www.freepik.com/free-photo/close-up-honeycomb-with-bee-honey-ai-generative_41367674.htm#query=bee%20hive%20image&position=6&from_view=search&track=ais</p>
+        <p>Top view of the honeycomb: https://www.freepik.com/free-photo/top-view-honeycomb_4539711.htm#query=bee%20hive%20image&position=0&from_view=search&track=ais</p>
+        <p>Zoomed in honeycomb: https://www.freepik.com/free-photo/yellow-honeycomb-texture_11042212.htm#query=bee%20hive%20image&position=21&from_view=search&track=ais</p>
     </div>)
 };
 
